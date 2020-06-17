d39f4c97b95da936859ee17c60440820cb68075d
ALI Akbar SABZI <57127882+sabzi1984@users.noreply.github.com>
2020-06-17 15:45:12 -0400

    Create mark component by BFS.py
    mark component by BFS without recursion

---
+++ mark component by BFS.py
@@ -0,0 +1,31 @@
+def mark_component(G, node, marked):
+    open_list = [node]
+    total_marked = [node]
+    marked[node] = True
+    while len(open_list) > 0:
+        node = open_list.pop(0)
+        for neighbor in G[node]:
+            if neighbor not in marked:
+                open_list.append(neighbor)
+                marked[neighbor] = True
+                total_marked.append(neighbor)
+    return total_marked
+#########
+# Code for testing
+#
+def make_link(G, node1, node2):
+    if node1 not in G:
+        G[node1] = {}
+    (G[node1])[node2] = 1
+    if node2 not in G:
+        G[node2] = {}
+    (G[node2])[node1] = 1
+    return G
+
+
+test_edges = [(1, 2), (2, 3),(3,4), (4, 5), (5, 6),(1,5),(1,6),(2,7)]
+G = {}
+for n1, n2 in test_edges:
+    make_link(G, n1, n2)
+marked = {}
+print(mark_component(G, 1, marked))
