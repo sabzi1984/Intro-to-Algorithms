fc41e05d613ff4dc1dfad2b7cf9fe4a9539a0c85
ALI Akbar SABZI <57127882+sabzi1984@users.noreply.github.com>
2020-06-23 20:29:01 -0400

    Update top K element.py

--- top K element.py
+++ top K element.py
@@ -15,7 +15,7 @@ def top_k(L,k):
     v=L[random.randrange(len(L))]
     (left,middle,right)=partition(L, v)
     if len(left)==k: return left
-    if len(left)==k+1: return left+middle
+    if len(left)+1==k: return left+middle
     if len(left)>k: return top_k(left,k)
     return left+middle+top_k((right),k-len(left)-1)
 L=[31,45,91,51,66,82,28,33,11,89,84,27,36]
