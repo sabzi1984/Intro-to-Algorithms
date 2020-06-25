e68ddaa5fbe46e1f33c310636cc88a6667a90480
ALI Akbar SABZI <57127882+sabzi1984@users.noreply.github.com>
2020-06-25 11:22:13 -0400

    Update down heapify.py

--- down heapify.py
+++ down heapify.py
@@ -1,13 +1,3 @@
-def remove_min(L):
-    if len(L)==1:
-        return[]
-    else:
-        L[0]=L.pop()
-    
-        down_heapify(L, 0)
-    return L
-    
-
 def parent(i): 
     return (i-1)/2
 def left_child(i): 
@@ -19,28 +9,20 @@ def is_leaf(L,i):
 def one_child(L,i): 
     return (left_child(i) < len(L)) and (right_child(i) >= len(L))
 
-# Call this routine if the heap rooted at i satisfies the heap property
-# *except* perhaps i to its immediate children
+
 def down_heapify(L, i):
-    # If i is a leaf, heap property holds
+    
     if is_leaf(L, i): 
         return
-    # If i has one child...
     if one_child(L, i):
-        # check heap property
         if L[i] > L[left_child(i)]:
-            # If it fails, swap, fixing i and its child (a leaf)
             (L[i], L[left_child(i)]) = (L[left_child(i)], L[i])
         return
-    # If i has two children...
-    # check heap property
+   
     if min(L[left_child(i)], L[right_child(i)]) >= L[i]: 
         return
-    # If it fails, see which child is the smaller
-    # and swap i's value into that child
-    # Afterwards, recurse into that child, which might violate
+   
     if L[left_child(i)] < L[right_child(i)]:
-        # Swap into left child
         (L[i], L[left_child(i)]) = (L[left_child(i)], L[i])
         down_heapify(L, left_child(i))
         return
@@ -48,12 +30,7 @@ def down_heapify(L, i):
         (L[i], L[right_child(i)]) = (L[right_child(i)], L[i])
         down_heapify(L, right_child(i))
         return
-
-#########
-# Testing Code
-#
-
-# build_heap
+    
 def build_heap(L):
     for i in range(len(L)-1, -1, -1):
         down_heapify(L, i)
@@ -62,4 +39,3 @@ def build_heap(L):
 
 L = list(range(10))
 print(build_heap(L))
-print(remove_min(L))
