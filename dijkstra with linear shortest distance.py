86ed43efa7df5a56e6c896b2cfe42bb6d7cc1c49
ALI Akbar SABZI <57127882+sabzi1984@users.noreply.github.com>
2020-07-01 13:03:02 -0400

    Create dijkstra with linear shortest distance.py
    Dijksra algorithm with linear shortest distance look-up. big tethe (n^2)

---
+++ dijkstra with linear shortest distance.py
@@ -0,0 +1,39 @@
+# look for shortest distance from the "dist_so_far" dictionary
+#and return the closest node
+def shortest_dist_node(dist):
+    best_node = None
+    best_value = 1000000
+    for v in dist:
+        if dist[v] < best_value:
+            (best_node, best_value) = (v, dist[v])
+    return best_node
+#add all the neighbour nodes and their distance to the "dist_so_far"  dictionary
+#fix the shrtest distance as the "final distance" dictionary
+def dijkstra(G,v):
+    dist_so_far = {}
+    dist_so_far[v] = 0
+    final_dist = {}
+    while len(final_dist) < len(G):
+        w = shortest_dist_node(dist_so_far)
+        final_dist[w] = dist_so_far[w]
+        del dist_so_far[w]
+        for x in G[w]:
+            if x not in final_dist:
+                if x not in dist_so_far:
+                    dist_so_far[x] = final_dist[w] + G[w][x]
+                elif final_dist[w] + G[w][x] < dist_so_far[x]:
+                    dist_so_far[x] = final_dist[w] + G[w][x]
+    return final_dist
+#make linke with distanceas as weights
+def make_link(G, node1, node2, w):
+    if node1 not in G:
+        G[node1] = {}
+    if node2 not in G[node1]:
+        (G[node1])[node2] = 0
+    (G[node1])[node2] += w
+    if node2 not in G:
+        G[node2] = {}
+    if node1 not in G[node2]:
+        (G[node2])[node1] = 0
+    (G[node2])[node1] += w
+    return G
